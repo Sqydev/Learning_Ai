0cf1559084a29be91296b3283f706542ce6dde86
Sqyd <wojkap10@proton.me>
2025-07-12 14:01:04 +0200

    Ended tensor section

--- basics/Tensors.py
+++ basics/Tensors.py
@@ -25,8 +25,12 @@ tensor_from_numpy_array = torch.from_numpy(numpy_array)
 print(f"Tensor from NumpyArray: \n {tensor_from_numpy_array} \n")
 
 
-# Create new Tensor that is basicly identical to source Tensor but with all the data set to 1. So src_tensor = ([3, 2], [4, 1]) -> new_tensor = ([1, 1], [1, 1])
+# Create new Tensor that is basicly identical to source Tensor but with all the data set to 1. So src_tensor = ([3, 2], [4, 1]) -> new_tensor = ([1, 1], [1, 1]) BUT you can also chainge aspects so you can
+# chainge from int to float like that tensor_with_all_data_as_one = torch.ones_like(tensor_from_data, dtype=torch.float) here's rest of things you can chainge: 
+# https://docs.pytorch.org/docs/stable/generated/torch.ones_like.html#torch.ones_like
 tensor_with_all_data_as_one = torch.ones_like(tensor_from_data)
+
+# And there's also zeros_like() that is litteraly ones_like() but with zeros
 
 # Print Tensor
 print(f"Ones Tensor: \n {tensor_with_all_data_as_one} \n")
@@ -38,3 +42,44 @@ tensor_with_random_data = torch.rand_like(tensor_from_data, dtype=torch.float)
 
 # Print Tensor
 print(f"Random Tensor: \n {tensor_with_random_data} \n")
+
+# You can also create tensor from shape like so:
+shape = (3, 2)
+
+shape_tensor = torch.ones(shape)
+print(f"Tensor from shape: \n {shape_tensor} \n")
+
+# Note: when you're making tensor from shape use torch.ones/zeros/rand itp. and NOT torch.ones_like/zeros_like/rand_like!!!!!!!!!
+
+# And you can also print idividual atrybutes of an tensor
+tensor_attributes_show = torch.tensor([[2, 5], [2, 5]])
+
+print(f"Shape of tensor_attributes_show: {tensor_attributes_show.shape}")
+print(f"Datatype of tensor_attributes_show: {tensor_attributes_show.dtype}")
+print(f"Device tensor_attributes_show is stored on: {tensor_attributes_show.device} \n")
+
+# From now on I'll use only tensor tensor
+tensor = torch.tensor([[1, 4, 2], [4, 6, 1]])
+
+# Also. you can do tensor operations like moving tensor to gpu
+if torch.cuda.is_available():
+  tensor = tensor.to('cuda')
+  print(f"Device tensor is stored on: {tensor.device} \n")
+
+# SLICING:)
+tensor = torch.ones(4, 4)
+tensor[:,1] = 0
+print(f"Fun slicing: {tensor} \n")
+
+# Also you can join, multyply, matrix_mul, in place operations, yare yare
+
+# Also tensor to numpy array:
+ten_to_numpy_array = tensor.numpy()
+print(f"Numpy arr from tensor: {ten_to_numpy_array} \n")
+
+# Also, numpy array from tensor is something like pointer cuz if src tensor chainges numpy array chainges too
+tensor.add_(1)
+print(f"src tensor after addition: {tensor} \n")
+print(f"numpy array without adding but made from tensor that is after addition: {ten_to_numpy_array} \n")
+
+# Btw it is the same with tensor from numpy array but i'm to lazy to do example for this
